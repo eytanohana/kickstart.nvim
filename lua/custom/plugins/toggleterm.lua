2328e219572093d34dafab9c6ccca89fefb50799
eytanohana <eohana98@gmail.com>
2023-07-27 02:08:09 +0300

    add toggleterm keymap

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,3 +1,25 @@
+local map = vim.keymap.set
+local toggle_term_group = vim.api.nvim_create_augroup( "toggle_term", { clear = true })
+
+vim.api.nvim_create_autocmd(
+    'FileType',
+    {
+        group = toggle_term_group,
+        pattern = 'toggleterm',
+        callback = function()
+            local opts = {noremap = true}
+            vim.api.nvim_buf_set_keymap(0, 't', '<A-3>', [[<C-\><C-n>:ToggleTerm<CR>]], opts)
+            vim.api.nvim_buf_set_keymap(0, 't', '<A-h>', [[<C-\><C-n><C-W>h]], opts)
+            vim.api.nvim_buf_set_keymap(0, 't', '<A-j>', [[<C-\><C-n><C-W>j]], opts)
+            vim.api.nvim_buf_set_keymap(0, 't', '<A-k>', [[<C-\><C-n><C-W>k]], opts)
+            vim.api.nvim_buf_set_keymap(0, 't', '<A-l>', [[<C-\><C-n><C-W>l]], opts)
+        end
+    }
+)
+
+map('n', '<A-3>', ':ToggleTerm<CR>')
+map('i', '<A-3>', '<ESC>:ToggleTerm<CR>')
+
 
 return {
   'akinsho/toggleterm.nvim',
