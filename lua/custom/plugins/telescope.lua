d19f2d02ff2e1e27c6c7a3acf1aed77250e06ada
eytanohana <eohana98@gmail.com>
2023-07-30 21:30:13 +0300

    search keymaps

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -34,6 +34,7 @@ return {
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch for [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
       builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
