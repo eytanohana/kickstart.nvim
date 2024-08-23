ae18227e92e40fbf9a63f7b668d5dfc20c1ae5d1
eytanohana <eohana98@gmail.com>
2024-08-24 01:16:54 +0300

    update neotree

--- lua/custom/plugins/neotree.lua
+++ lua/custom/plugins/neotree.lua
@@ -1,26 +1,28 @@
--- Unless you are still migrating, remove the deprecated commands from v1.x
-vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
-
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
-  config = function ()
-    require('neo-tree').setup({
+  config = function()
+    require('neo-tree').setup {
+      close_if_last_window = true,
       filesystem = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
         window = {
           mappings = {
             ['l'] = 'open',
-            ['h'] = 'close_node'
-          }
-        }
-      }
-    })
+            ['h'] = 'close_node',
+          },
+        },
+      },
+    }
     vim.keymap.set('n', '<A-0>', ':Neotree toggle reveal_force_cwd<CR>', { silent = true })
   end,
 }
-
