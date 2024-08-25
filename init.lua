c36027f1210e47deed1906c03b59c781a615e26b
eytanohana <eohana98@gmail.com>
2024-08-25 07:55:00 +0300

    sig help

--- init.lua
+++ init.lua
@@ -415,6 +415,8 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('gs', vim.lsp.buf.signature_help, '[G]et [S]ignature Documentation')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
